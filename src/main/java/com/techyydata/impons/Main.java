f38b2a27a37a47287d9f6376264edd7a2270a7ca
TechyyData <34737526+TechyyData@users.noreply.github.com>
2018-02-27 19:41:06 -0600

    Application Help

--- src/main/java/com/techyydata/impons/Main.java
+++ src/main/java/com/techyydata/impons/Main.java
@@ -1,13 +1,29 @@
 package com.techyydata.impons;
 
+import com.mrcrayfish.device.api.ApplicationManager;
+import com.techyydata.impons.apps.ApplicationScriptWriter;
 import com.techyydata.impons.util.Reference;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION, acceptedMinecraftVersions = Reference.ACCEPTED_VERSIONS, dependencies = Reference.DEPENDS)
 public class Main
 {
+
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
     public void init(FMLInitializationEvent event)
+    {
+        ApplicationManager.registerApplication(new ResourceLocation(Reference.MOD_ID, "script_writer"), ApplicationScriptWriter.class);
+    }
+
+    public void postInit(FMLPostInitializationEvent event)
     {
 
     }
---
+++ src/main/java/com/techyydata/impons/apps/ApplicationScriptWriter.java
@@ -0,0 +1,27 @@
+package com.techyydata.impons.apps;
+
+/*
+ * Script Writer Application class
+ */
+
+import com.mrcrayfish.device.api.app.Application;
+import net.minecraft.nbt.NBTTagCompound;
+
+public class ApplicationScriptWriter extends Application {
+
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void load(NBTTagCompound nbtTagCompound) {
+
+    }
+
+    @Override
+    public void save(NBTTagCompound nbtTagCompound) {
+
+    }
+}
