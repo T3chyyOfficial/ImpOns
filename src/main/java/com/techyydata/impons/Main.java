ca59fb34ff205c3623acd393341c6232c5da72e2
TechyyData <34737526+TechyyData@users.noreply.github.com>
2018-02-27 18:20:59 -0600

    Add files

---
+++ src/main/java/com/techyydata/impons/Main.java
@@ -0,0 +1,15 @@
+package com.techyydata.impons;
+
+import com.techyydata.impons.util.Reference;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = Reference.MOD_ID, name = Reference.NAME, version = Reference.VERSION, acceptedMinecraftVersions = Reference.ACCEPTED_VERSIONS, dependencies = Reference.DEPENDS)
+public class Main
+{
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+}
---
+++ src/main/java/com/techyydata/impons/util/Reference.java
@@ -0,0 +1,12 @@
+package com.techyydata.impons.util;
+
+public class Reference
+{
+    public static final String MOD_ID = "impons";
+    public static final String NAME = "Imp-Ons";
+    public static final String VERSION = "2.1-beta";
+    public static final String ACCEPTED_VERSIONS = "[1.12.2]";
+    public static final String DEPENDS = "required-after:cdm@[0.2.1,)";
+
+}
+
