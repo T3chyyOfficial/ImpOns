c5f25339992cd9c0fdcaed9818ff82572a068366
TechyyData <34737526+TechyyData@users.noreply.github.com>
2018-03-06 22:41:28 -0600

    Document Writer has a save feature

--- src/main/java/com/techyydata/impons/apps/ApplicationDocumentWriter.java
+++ src/main/java/com/techyydata/impons/apps/ApplicationDocumentWriter.java
@@ -17,23 +17,34 @@ public class ApplicationDocumentWriter extends Application {
     private Label creditLabel;
     private TextArea scriptArea;
     private TextField scriptTitle;
+    private Button saveButton;
 
     //Application Components
     @Override
     public void init() {
-        scriptLayout = new Layout(0, 0, 225, 120);
+        scriptLayout = new Layout(0, 0, 225, 130);
         this.setCurrentLayout(scriptLayout);
 
         creditLabel = new Label("Document Writer was created by TechyyMC", 2, 95);
         scriptLayout.addComponent(creditLabel);
 
-        scriptArea = new TextArea(4, 20, 180, 70);
+        scriptArea = new TextArea(4, 20, 200, 70);
         scriptLayout.addComponent(scriptArea);
 
-        scriptTitle = new TextField(4, 3, 180);
+        scriptTitle = new TextField(4, 3, 200);
         this.scriptTitle.setPlaceholder("Title of Document");
         this.scriptTitle.setIcon(Icons.EDIT);
         scriptLayout.addComponent(scriptTitle);
+
+        saveButton = new Button(1, 100, "Save");
+        saveButton.setSize(63, 20);
+        saveButton.setClickListener((mouseX, mouseY, mouseButton) -> {
+            if (mouseButton == 0) {
+                this.openDialog(new Dialog.SaveFile(this, new NBTTagCompound()));
+            }
+        });
+        scriptLayout.addComponent(saveButton);
+
     }
 
     @Override
