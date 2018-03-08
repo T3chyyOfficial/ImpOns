82471fbf76dd3a105f259d4e55877a6b3e581827
TechyyData <34737526+TechyyData@users.noreply.github.com>
2018-03-08 17:26:44 -0600

    Exception Message

---
+++ src/main/java/com/techyydata/impons/LaunchException.java
@@ -0,0 +1,9 @@
+package com.techyydata.impons;
+
+public class LaunchException extends Exception {
+    
+    @Override
+    public String getMessage() {
+        return "The developer version of Imp-Ons has been detected and can be only ran in a forge development environment, If you are not a developer, Download the original Imp-Ons here: https://minecraft.curseforge.com/projects/imp-ons";
+    }
+}
